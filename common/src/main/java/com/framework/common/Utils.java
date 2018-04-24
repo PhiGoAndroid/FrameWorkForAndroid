aa0098dd1916d221c9aa1f689fd9f8cde25612e5
张亚东 <yadong.zhang@phicomm.com>
2018-04-24 13:42:11 +0800

    新增日志

---
+++ common/src/main/java/com/framework/common/Utils.java
@@ -0,0 +1,15 @@
+package com.framework.common;
+
+import android.util.Log;
+
+/**
+ * Created by zhangyadong on 2018/4/24.
+ */
+
+public class Utils {
+
+    public static void outLayout(String loginfo) {
+        Log.i("Utils", loginfo);
+    }
+
+}
